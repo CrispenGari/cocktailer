3bf0e15309d000e4493b62c5ce002628d380f832
CrispenGari <crispengari@gmail.com>
2024-10-09 22:12:42 +0200

    search cocktails

--- mobile/app/(modals)/search-favorites.tsx
+++ mobile/app/(modals)/search-favorites.tsx
@@ -1,11 +1,60 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
+import { useSearchTermsStore } from "@/store/searchTerms";
+import { COLORS, FONTS } from "@/constants";
+import { useFavoritesStore } from "@/store/favoritesStore";
+import { FavoriteCocktail } from "@/components/Cocktail";
 
 const Page = () => {
+  const { query } = useSearchTermsStore();
+
+  const { favorites: cocktails } = useFavoritesStore();
+  const results = cocktails.filter((c) =>
+    c.name.toLowerCase().includes(query.favorites.trim().toLowerCase())
+  );
+
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <ScrollView
+      style={{ flex: 1, backgroundColor: COLORS.primary, padding: 10 }}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{ paddingBottom: 100 }}
+    >
+      {!!query.favorites ? (
+        results.length === 0 ? (
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              padding: 10,
+            }}
+          >
+            <Text style={{ fontFamily: FONTS.regular, textAlign: "center" }}>
+              No matches for the query "{query.favorites}".
+            </Text>
+          </View>
+        ) : (
+          results.map((cocktail, index) => (
+            <FavoriteCocktail
+              index={index}
+              key={index}
+              cocktail={cocktail as any}
+            />
+          ))
+        )
+      ) : (
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            padding: 10,
+          }}
+        >
+          <Text style={{ fontFamily: FONTS.regular, textAlign: "center" }}>
+            Search Favorite Cocktails
+          </Text>
+        </View>
+      )}
+    </ScrollView>
   );
 };
 
